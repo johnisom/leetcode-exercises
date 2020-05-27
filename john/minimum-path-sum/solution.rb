8dc93c224a94c0d39751f173bcf6a2f0ccc68efd
John Isom <John.Isom2001@gmail.com>
2020-05-27 12:17:46 -0600

    Add bottom-up solution to minimum-path-sum

--- john/minimum-path-sum/solution.rb
+++ john/minimum-path-sum/solution.rb
@@ -1,4 +1,5 @@
-# require 'pry'
+=begin
+# The top-down solution
 
 def min_path_sum(grid)
   cache = Array.new(grid.size) do
@@ -36,15 +37,30 @@ def min_sum_helper(grid, row, col, cache)
   min_down_right = [cache[row][col + 1], cache[row + 1][col]].min
   return grid[row][col] + min_down_right
 end
+=end
+# Bottom-up solution
 
-test1 = [
-  [1, 3, 1],
-]
+def min_path_sum(grid)
+  cache = Array.new(grid.size) { Array.new(grid.first.size) }
 
-test2 = [
-  [1,3,1],
-  [1,5,1],
-  [4,2,1]
-]
+  (grid.size - 1).downto(0) do |row|
+    (grid.first.size - 1).downto(0) do |col|
+      current_value = grid[row][col]
+      right_adjacent = cache[row][col + 1]
+      down_adjacent = cache[row + 1][col] if row != grid.size - 1
 
-puts min_path_sum(test2)
+      cache[row][col] = current_value + if row == grid.size - 1 &&
+                                           col == grid.first.size - 1
+                                          0
+                                        elsif row == grid.size - 1
+                                          right_adjacent
+                                        elsif col == grid.first.size - 1
+                                          down_adjacent
+                                        else
+                                          [right_adjacent, down_adjacent].min
+                                        end
+    end
+  end
+
+  cache.first.first
+end
